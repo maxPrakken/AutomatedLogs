301b8bc4b1508c9c0f1ad2f6bb57b161e7bea323
maxPrakken <maxprakken3@hotmail.nl>
2019-03-12 14:36:31 +0100

    added comments

--- AutomatedLogs.c
+++ AutomatedLogs.c
@@ -6,7 +6,7 @@
 #include <time.h>
 #include <stdbool.h>
 
-typedef struct {
+typedef struct { // activity struct, has 4 variables that determen what you did and when
 	bool active;
 	char name[150];
 	char timeStart[15];
@@ -14,78 +14,78 @@ typedef struct {
 }ACTIVITY;
 
 ACTIVITY activities[20]; // max of 20 activities a day, i think you don't do much more than that in total
-int aCounter = 0;
+int aCounter = 0; // counter for activities array
 
-time_t secs;
-struct tm *local;
-char timeString[15];
+time_t secs; // time from epoch in secs
+struct tm *local; // local time info
+char timeString[15]; // char that gets time pushed in as char
 
-void Startd(), Add(), Stop(), Folder(), SaveCur(), GetTimeH();
+void Startd(), Add(), Stop(), Folder(), SaveCur(), GetTimeH(); // void functions
 
 int main() {
 	
-	secs = time(0);
-	local = localtime(&secs);
+	secs = time(0); // declare secs on epoch 
+	local = localtime(&secs); // get localtime with help of secs
 
-	strftime(timeString, sizeof(timeString), "%H:%M:%S", local);
+	strftime(timeString, sizeof(timeString), "%H:%M:%S", local); // converts time in hours, minutes, seconds in the timestring as chars
 
 	//printf("now: %d-%d-%d %d:%d:%d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 
-	char* f = malloc(7);
-	char path[100];
+	char* f = malloc(7); // char that holds the called function
+	char path[100]; // path you can set through Folder(), max of 100 chars, dont think you need much more
 
-	char start[] = "start",
+	char start[] = "start", 
 		add[] = "add",
 		stop[] = "stop",
 		folder[] = "folder",
 		savecur[] = "savecur";
 
-	while (strcmp(f, stop) != 0) {
-		gets(f);
+	while (strcmp(f, stop) != 0) { // if command is not stop
+		gets(f); // put console input in f
 		
-		if (strcmp(f, start) == 0) {
-			Startd();
+		if (strcmp(f, start) == 0) { // checks if f equals start
+			Startd(); // calls startd function
 		}
-		else if (strcmp(f, add) == 0) {
-			Add();
+		else if (strcmp(f, add) == 0) { // checks if f equals add
+			Add(); // calls add function
 		}
-		else if (strcmp(f, folder) == 0) {
-			Folder(path);
+		else if (strcmp(f, folder) == 0) { // checks if f equals folder
+			Folder(path); // calls folder function and passes the path variable
 		}
-		else if (strcmp(f, savecur) == 0) {
-			SaveCur();
+		else if (strcmp(f, savecur) == 0) { // checks if f equals savecur
+			SaveCur(); // calls savecur function
 		}
-		else if (strcmp(f, "help") == 0) {
+		else if (strcmp(f, "help") == 0) { // checks if f equals help
 			printf("\ncommands:\nstart : 'takes one argument, argument is\n the activity you're about to start.\n\nadd : takes three arguments, first is the activity, \nsecond is time started, third is time it ended.\n\nstop : has two options, 'current' and 'day', \ncurrent stops current activity, day stops the day.\n\nfolder : takes one argument, path where you want your .txt's and .pdf's stored.\n");
-			fflush(stdout);
+			fflush(stdout); // instantly prints anything left on stdout aka the printf
 		}
-		else if(strcmp(f, "") == 0 || strcmp(f, stop)) {
+		else if(strcmp(f, "") == 0 || strcmp(f, stop)) { // checks if f equals stop
 			printf("\nnot a valid command, type 'help' for more information\n");
-			fflush(stdout);
+			fflush(stdout); // instantly prints anything left on stdout aka the printf
 		}
 	}
 
-	free(f);
+	free(f); // frees f variable from the stack
 
-	return 0;
+	return 0; // stops program
 }
 
 void Startd() {
 	
-	fflush(stdout);
+	fflush(stdout); // flushes stdout just in case
 
 	printf("what activity do you want to start? : ");
-	fflush(stdout);
+	fflush(stdout); // instantly prints stdout
 
-	activities[aCounter].active = true;
-	gets(activities[aCounter].name);
+	activities[aCounter].active = true; // sets the current activity to true
+	gets(activities[aCounter].name); // gets and sets activity name from the console
 
-	GetTimeH();
-	strcpy(activities[aCounter].timeStart, timeString);
+	GetTimeH(); // refreshes time in timeString
+	strcpy(activities[aCounter].timeStart, timeString); // copies timestring to activity timestart
 	printf("\nActivity started at: ");
 	printf(timeString);
 	printf("\n");
-	fflush(stdout);
+	fflush(stdout); // instantly prints stdout
 }
 
 void Add() {
@@ -97,25 +97,25 @@ void Stop() {
 }
 
 void Folder(char path[]) {
-	gets(path);
+	gets(path); // gets and sets path from console
 	printf("\nFolder has been set to: ");
 	printf(path);
 	printf("\n");
-	fflush(stdout);
+	fflush(stdout); // instantly prints stdout
 }
 
 void SaveCur() {
-	GetTimeH();
+	GetTimeH(); // refreshes timeString
 	printf("activity ");
 	printf(activities[aCounter].name);
 	printf(" stopped.\n");
-	strcpy(activities[aCounter].timeStop, timeString);
-	fflush(stdout);
-	aCounter = aCounter + 1;
+	strcpy(activities[aCounter].timeStop, timeString); // copies timeString into activity timestop
+	fflush(stdout); // instantly prints stdout
+	aCounter = aCounter + 1; // adds one to aCounter
 }
 
 void GetTimeH() {
-	secs = time(0);
-	local = localtime(&secs);
-	strftime(timeString, sizeof(timeString), "%H:%M:%S", local);
+	secs = time(0); // sets secs back to epoch
+	local = localtime(&secs); // refreshes localtime
+	strftime(timeString, sizeof(timeString), "%H:%M:%S", local); // turn localtime into timestring
 }
